fd5cbe3c50a92e845559b34f347a63ca8494021b
yimjohns <yimjohns.dev@gmail.com>
2021-02-04 14:19:43 +0100

    Add list extraction

---
+++ listextraction.py
@@ -0,0 +1,17 @@
+num_list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+# Extracting 5
+print(num_list[5])
+
+# Extracting 7
+print(num_list[7])
+
+# Extracting 9
+print(num_list[9])
+
+# Extracting 2
+print(num_list[2])
+
+
+
+
