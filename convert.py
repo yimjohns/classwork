8cefdcf52e06c71642b28bfc2302432ca33a2902
yimjohns <yimjohns.dev@gmail.com>
2021-02-04 14:28:59 +0100

    Question 5 conversion from list to set and tuple modified.

--- convert.py
+++ convert.py
@@ -3,3 +3,8 @@ list_num = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 set_converted = set(list_num)
 tuple_converted = tuple(list_num)
 
+print("The list: {}".format(list_num))
+
+print("Converted to Set = {}".format(set_converted))
+
+print("Converted to tuple = {}".format(tuple_converted))
